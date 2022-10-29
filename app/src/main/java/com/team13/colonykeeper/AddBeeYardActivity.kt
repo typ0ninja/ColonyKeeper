e8caeca9105c0c50bd044c38c2fad1cd9754e7c2
Tanner Dreiling <tdreiling19@utexas.edu>
2022-10-29 15:13:02 -0500

    AddBeeYard now displays small image taken

--- app/src/main/java/com/team13/colonykeeper/AddBeeYardActivity.kt
+++ app/src/main/java/com/team13/colonykeeper/AddBeeYardActivity.kt
@@ -2,6 +2,7 @@ package com.team13.colonykeeper
 
 import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +13,8 @@ import com.team13.colonykeeper.databinding.ActivityMainBinding
 class AddBeeYardActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddBeeYardBinding
+    private val pic_id = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityAddBeeYardBinding.inflate(layoutInflater)
@@ -33,6 +36,16 @@ class AddBeeYardActivity : AppCompatActivity() {
         } catch (e: ActivityNotFoundException) {
             // display error state to the user
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode == pic_id){
+            val photo = data!!.extras!!["data"] as Bitmap?
+            binding.beeYardPicture.setImageBitmap(photo)
+
+        }
+
     }
 
 
