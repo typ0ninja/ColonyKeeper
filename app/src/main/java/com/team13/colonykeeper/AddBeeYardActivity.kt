ba7966b37754ea4a86fd793f281bc82a1614635b
Tanner Dreiling <tdreiling19@utexas.edu>
2022-11-06 17:04:51 -0600

    Finally displays camera intent photo onto screen

--- app/src/main/java/com/team13/colonykeeper/AddBeeYardActivity.kt
+++ app/src/main/java/com/team13/colonykeeper/AddBeeYardActivity.kt
@@ -1,13 +1,11 @@
 package com.team13.colonykeeper
 
-import android.content.ActivityNotFoundException
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.provider.MediaStore
 import android.util.Log
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.FileProvider
 import com.team13.colonykeeper.databinding.ActivityAddBeeYardBinding
@@ -21,7 +19,7 @@ class AddBeeYardActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityAddBeeYardBinding
     private val pic_id = 1
-    lateinit var mUri: Uri
+    var cameraPhotoFilePath: Uri? = null
     private lateinit var imageFilePath: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,24 +39,23 @@ class AddBeeYardActivity : AppCompatActivity() {
         val REQUEST_IMAGE_CAPTURE = 1
         val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
 
-        if(takePictureIntent.resolveActivity(getPackageManager()) != null){
             //Create a file to store the image
             var photoFile: File? = null;
             try {
                 photoFile = createImageFile();
                 Log.d("erroring", "${photoFile}")
             } catch (e: IOException) {
-
+                Log.d("erroring", "didn't make the file")
             }
             if (photoFile != null) {
                 Log.d("erroring", "didn't make the file")
-                var photoURI = FileProvider.getUriForFile(this,                                                                                                "com.example.android.provider", photoFile);
+                cameraPhotoFilePath = FileProvider.getUriForFile(Objects.requireNonNull(getApplicationContext()),
+                    BuildConfig.APPLICATION_ID + ".provider", photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT,
-                    photoURI);
+                    cameraPhotoFilePath);
                 startActivityForResult(takePictureIntent,
                     REQUEST_IMAGE_CAPTURE);
             }
-        }
 
 
 //        try {
@@ -87,9 +84,9 @@ class AddBeeYardActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
         super.onActivityResult(requestCode, resultCode, data)
         if(requestCode == pic_id){
-            val uri = data?.getData()
-            binding.beeYardPicture.setImageURI(null)
-            binding.beeYardPicture.setImageURI(uri)
+            val bitmap = MediaStore.Images.Media.getBitmap(this.contentResolver, cameraPhotoFilePath)
+
+           binding.beeYardPicture.setImageBitmap(bitmap)
 //            val photo = data!!.extras!!["data"] as Bitmap?
 //            binding.beeYardPicture.setImageBitmap(photo)
 
