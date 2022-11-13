0d541849cd06faff2c1322faed5e0136f5f1a2b2
tipp0 <sh0cwave!>
2022-11-12 20:10:46 -0600

    base setup for pdf export and some bug fixes

--- app/src/main/java/com/team13/colonykeeper/ReportActivity.kt
+++ app/src/main/java/com/team13/colonykeeper/ReportActivity.kt
@@ -1,18 +1,44 @@
 package com.team13.colonykeeper
 
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Typeface
+import android.graphics.pdf.PdfDocument
+import android.graphics.pdf.PdfDocument.PageInfo
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.team13.colonykeeper.database.ColonyApplication
+import com.team13.colonykeeper.database.ColonyViewModel
+import com.team13.colonykeeper.database.ColonyViewModelFactory
+import com.team13.colonykeeper.database.Inspections
 import com.team13.colonykeeper.databinding.ActivityReportBinding
+
 
 class ReportActivity: AppCompatActivity() {
     private lateinit var binding: ActivityReportBinding
 
+    private val colonyViewModel: ColonyViewModel by viewModels {
+        ColonyViewModelFactory((application as ColonyApplication).colonyRepository)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityReportBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         supportActionBar?.title = "Generate Report"
+
+        var inspections: List<Inspections> = emptyList()
+
+        colonyViewModel.getInspections()
+            .observe(this) {
+                inspections = it
+            }
+    }
+
+
+    fun makePdf(){
+
     }
 }
