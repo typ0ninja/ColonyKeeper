a6aadb077663cabd84fff7f9ea11477b494ec04f
Scarlett Shires <scarlettshires@utexas.edu>
2022-11-13 20:21:32 -0600

    Done except for figuring out suspend

--- app/src/main/java/com/team13/colonykeeper/adapter/FutureInspectionsChildAdapter.kt
+++ app/src/main/java/com/team13/colonykeeper/adapter/FutureInspectionsChildAdapter.kt
@@ -1,15 +1,27 @@
 package com.team13.colonykeeper.adapter
 
+import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.CheckBox
+import android.widget.ImageButton
+import android.widget.PopupMenu
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 import com.team13.colonykeeper.R
+import com.team13.colonykeeper.database.ColonyApplication
 import com.team13.colonykeeper.database.Scheduled
+import com.team13.colonykeeper.workers.InspectionNotificationWorker
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
 
-class FutureInspectionsChildAdapter(var memberData: List<Scheduled>) :
+class FutureInspectionsChildAdapter(private val context: Context?, var memberData: List<Scheduled>) :
     RecyclerView.Adapter<FutureInspectionsChildAdapter.ScheduledViewHolder>() {
 
     private var scheduledList: List<Scheduled> = mutableListOf()
@@ -23,12 +35,14 @@ class FutureInspectionsChildAdapter(var memberData: List<Scheduled>) :
         var inspectionDate: TextView
         var inspectionTime: TextView
         var isNotificationCheckBox: CheckBox
+        var optionButton: ImageButton
 
         init {
             locationName = itemView.findViewById(R.id.location_name)
             inspectionDate = itemView.findViewById(R.id.inspection_date)
             inspectionTime = itemView.findViewById(R.id.inspection_time)
             isNotificationCheckBox = itemView.findViewById(R.id.is_notification_check_box)
+            optionButton = itemView.findViewById(R.id.options_button)
         }
     }
 
@@ -40,12 +54,81 @@ class FutureInspectionsChildAdapter(var memberData: List<Scheduled>) :
             )
         )
 
-    override fun onBindViewHolder(holder: FutureInspectionsChildAdapter.ScheduledViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ScheduledViewHolder, position: Int) {
         holder.locationName.text = scheduledList[position].locName
         holder.inspectionDate.text = scheduledList[position].date
         holder.inspectionTime.text = scheduledList[position].time
         holder.isNotificationCheckBox.isChecked = scheduledList[position].isNotification
+
+        holder.isNotificationCheckBox.setOnClickListener{
+            toggleNotification(holder.isNotificationCheckBox.isChecked, scheduledList[position])
+        }
+        holder.optionButton.setOnClickListener {
+            var popupMenu = PopupMenu(context, holder.optionButton)
+            popupMenu.inflate(R.menu.popup_menu)
+            popupMenu.setOnMenuItemClickListener {_ ->
+                deleteScheduled(scheduledList[position])
+                true
+            }
+            popupMenu.show()
+        }
     }
 
     override fun getItemCount(): Int = scheduledList.size
+
+    private fun toggleNotification(isCurrentlyNotif: Boolean, scheduled: Scheduled) {
+//        if (isCurrentlyNotif){
+//            WorkManager.getInstance(context!!).cancelWorkById(UUID.fromString(scheduled.name))
+//            ColonyApplication.instance.colonyRepository.updateInspection(
+//                Scheduled(
+//                    "",
+//                    scheduled.date,
+//                    scheduled.time,
+//                    scheduled.tag,
+//                    scheduled.yardID,
+//                    scheduled.locName,
+//                    false
+//                )
+//            )
+//        } else {
+//            val myWorkRequest = OneTimeWorkRequestBuilder<InspectionNotificationWorker>()
+//                .setInitialDelay(getTimeDiff(scheduled.date + "T" + scheduled.time + "Z"), TimeUnit.SECONDS)
+//                .setInputData(
+//                    workDataOf(
+//                        "title" to "Time to Inspect Hives",
+//                        "message" to "Check ColonyKeeper to start inspection",
+//                    )
+//                )
+//                .build()
+//
+//            WorkManager.getInstance(context!!).enqueue(myWorkRequest)
+//            ColonyApplication.instance.colonyRepository.updateInspection(
+//                Scheduled(
+//                    myWorkRequest.id.toString(),
+//                    scheduled.date,
+//                    scheduled.time,
+//                    scheduled.tag,
+//                    scheduled.yardID,
+//                    scheduled.locName,
+//                    true
+//                )
+//            )
+//        }
+    }
+
+    private fun getTimeDiff(dateTime: String): Long {
+        val todayDateTime = Calendar.getInstance()
+        val format = SimpleDateFormat("yyyy-MM-dd'T'HH:mm'Z'");
+        val scheduledDateTime = format.parse(dateTime)
+        val cal = Calendar.getInstance()
+        cal.time = scheduledDateTime
+        return (cal.timeInMillis/1000L) - (todayDateTime.timeInMillis/1000L)
+    }
+
+    private fun deleteScheduled(scheduled: Scheduled) {
+        if (scheduled.isNotification){
+            WorkManager.getInstance(context!!).cancelWorkById(UUID.fromString(scheduled.name))
+        }
+//        ColonyApplication.instance.colonyRepository.deleteScheduled(scheduled.id)
+    }
 }
--- app/src/main/java/com/team13/colonykeeper/adapter/FutureInspectionsParentAdapter.kt
+++ app/src/main/java/com/team13/colonykeeper/adapter/FutureInspectionsParentAdapter.kt
@@ -5,7 +5,9 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.team13.colonykeeper.R
@@ -17,10 +19,14 @@ RecyclerView.Adapter<FutureInspectionsParentAdapter.YardInspectionViewHolder>() 
     class YardInspectionViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val locationTextView: TextView
         val childRecyclerView: RecyclerView
+        val expandButtonView: ImageButton
+        val expandedLayout: ConstraintLayout
 
         init {
             locationTextView = itemView.findViewById(R.id.location_name)
             childRecyclerView = itemView.findViewById(R.id.hive_inspection_list)
+            expandButtonView = itemView.findViewById(R.id.expand_button)
+            expandedLayout = itemView.findViewById(R.id.expanded_layout)
         }
     }
 
@@ -36,17 +42,27 @@ RecyclerView.Adapter<FutureInspectionsParentAdapter.YardInspectionViewHolder>() 
 
     override fun onBindViewHolder(viewHolder: YardInspectionViewHolder, position: Int) {
         viewHolder.locationTextView.text = yardList[position].yard.yardName
-        val childMembersAdapter = FutureInspectionsChildAdapter(yardList[position].ScheduledInspections)
+        val childMembersAdapter = FutureInspectionsChildAdapter(context, yardList[position].ScheduledInspections)
         viewHolder.childRecyclerView.layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL,false)
         viewHolder.childRecyclerView.adapter = childMembersAdapter
+
+        viewHolder.expandButtonView.setOnClickListener { toggleExpandedView(viewHolder) }
     }
 
     override fun getItemCount(): Int = yardList.size
 
     fun addData(list: List<YardInspection>) {
-        Log.d("ParentAdapter", "Here in addData")
-        Log.d("ParentAdapter", "List size ${list.size}")
         yardList = list
         notifyDataSetChanged()
+    }
+
+    private fun toggleExpandedView(viewHolder: YardInspectionViewHolder) {
+        if (viewHolder.expandedLayout.visibility == View.VISIBLE) {
+            viewHolder.expandedLayout.visibility = View.GONE;
+            viewHolder.expandButtonView.setImageResource(R.drawable.ic_baseline_expand_more_24);
+        } else {
+            viewHolder.expandedLayout.visibility = View.VISIBLE;
+            viewHolder.expandButtonView.setImageResource(R.drawable.ic_baseline_expand_less_24);
+        }
     }
 }
